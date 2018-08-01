31963586dcae16c84fc77ce8b32dd4bf0a900f31
unknown <766167441@qq.com>
2018-08-01 23:30:37 +0800

    Initial commit

---
+++ src/main/java/com/soonelu/pesonal_data_manager/PesonalDataManagerApplication.java
@@ -0,0 +1,12 @@
+package com.soonelu.pesonal_data_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PesonalDataManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PesonalDataManagerApplication.class, args);
+	}
+}
